9ba37d3593f86d9e5f3e883a4a22f89c2ce099a5
imba97 <mail@imba97.cn>
2022-05-08 23:57:49 +0800

    perf: 修改版本号

--- webpack.config.ts
+++ webpack.config.ts
@@ -19,7 +19,7 @@ module.exports = (): webpack.Configuration => {
   let manifestJSON = require('./src/manifest.json')
 
   // 版本号
-  manifestJSON.version = '2.0.5'
+  manifestJSON.version = '2.1.0'
 
   let configs: webpack.Configuration = {
     node: false,
