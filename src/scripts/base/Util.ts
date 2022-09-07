11d0a48cb324fce03fb627fed5ac9e23a046cd69
imba97 <mail@imba97.cn>
2022-09-07 23:10:26 +0800

    style(): 文件名大写开头

---
+++ src/scripts/base/Util.ts
@@ -0,0 +1,246 @@
+import _ from 'lodash'
+import Singleton from '@/scripts/base/singletonBase/Singleton'
+import ExtStorage from '@/scripts/base/storage/ExtStorage'
+import { TMultipleAccounts, IMultipleAccounts } from '@/scripts/base/storage/template/TMultipleAccounts'
+
+export default class Util extends Singleton {
+  private bvTool = {
+    table: [...'fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF'],
+    s: [11, 10, 3, 8, 4, 6],
+    xor: 177451812,
+    add: 8728348608
+  }
+
+  /**
+   * AV号 转 BV号
+   * @param av AV号
+   * @param isLower 开头的 BV 是否小写，默认为 false
+   * @returns BV 号
+   */
+  public av2bv(av: number, isLower = false): string {
+    let num = NaN
+    if (Object.prototype.toString.call(av) === '[object Number]') {
+      num = av
+    } else if (Object.prototype.toString.call(av) === '[object String]') {
+      num = parseInt(av.toString().replace(/[^0-9]/gu, ''))
+    }
+    if (isNaN(num) || num <= 0) {
+      throw new Error('¿你在想桃子?')
+    }
+
+    num = (num ^ this.bvTool.xor) + this.bvTool.add
+    const result = [...`${isLower ? 'bv' : 'BV'}1  4 1 7  `]
+    let i = 0
+    while (i < 6) {
+      result[this.bvTool.s[i]] =
+        this.bvTool.table[Math.floor(num / 58 ** i) % 58]
+      i += 1
+    }
+    return result.join('')
+  }
+
+  /**
+   * BV号 转 AV号
+   * @param bv AV号
+   * @returns AV 号
+   */
+  public bv2av(bv: string): number {
+    let str = ''
+    if (bv.length === 12) {
+      str = bv
+    } else if (bv.length === 10) {
+      str = `BV${bv}`
+    } else if (bv.length === 9) {
+      str = `BV1${bv}`
+    } else {
+      throw new Error('¿你在想桃子?')
+    }
+    if (
+      !str.match(
+        /[Bb][Vv][fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF]{10}/gu
+      )
+    ) {
+      throw new Error('¿你在想桃子?')
+    }
+
+    let result = 0
+    let i = 0
+    while (i < 6) {
+      result += this.bvTool.table.indexOf(str[this.bvTool.s[i]]) * 58 ** i
+      i += 1
+    }
+    return (result - this.bvTool.add) ^ this.bvTool.xor
+  }
+
+  public getElement(selector: string): Promise<HTMLElement> {
+    // 先获取一次
+    let element: HTMLElement | null = document.querySelector(selector)
+
+    if (element) return Promise.resolve(element)
+
+    // 如果没获取到 开启计时器 循环获取
+    return new Promise((resolve, reject) => {
+      let timeout = 120
+
+      const timer = setInterval(() => {
+        element = document.querySelector(selector)
+
+        // 成功获取
+        if (element !== null) {
+          resolve(element)
+          clearInterval(timer)
+        }
+
+        // timeout
+        if (timeout === 0) {
+          reject(new Error('Empty Element'))
+          clearInterval(timer)
+        }
+
+        timeout--
+      }, 500)
+    })
+  }
+
+  /**
+   * 获取页面上的元素，一分钟内如果没获取到则停止获取
+   * @param selector 选择器
+   */
+  public getElements(selector: string): Promise<NodeListOf<HTMLElement>> {
+    let elements: NodeListOf<HTMLElement> = document.querySelectorAll(selector)
+
+    if (elements.length > 0) return Promise.resolve(elements)
+
+    return new Promise((resolve, reject) => {
+      let timeout = 120
+      const timer = setInterval(() => {
+        elements = document.querySelectorAll(selector)
+
+        // 成功获取
+        if (elements.length !== 0) {
+          resolve(elements)
+          clearInterval(timer)
+        }
+
+        // timeout
+        if (timeout === 0) {
+          reject(new Error('Empty Element'))
+          clearInterval(timer)
+        }
+
+        timeout--
+      }, 500)
+    })
+  }
+
+  public random(length = 8, type?: string): string {
+    let chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    if (typeof type !== 'undefined') {
+      switch (type) {
+        case 'string':
+          chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+          break
+
+        case 'number':
+          chars = '0123456789'
+          break
+
+        default:
+          break
+      }
+    }
+
+    let result = ''
+
+    for (let i = length; i > 0; --i) {
+      result += chars[Math.floor(Math.random() * chars.length)]
+    }
+
+    return result
+  }
+
+  public inNodeList(
+    element: HTMLElement,
+    nodeList: NodeListOf<HTMLElement>
+  ): number {
+    let result = -1
+
+    nodeList.forEach((nodeItem, index) => {
+      if (element === nodeItem) {
+        result = index
+        return false
+      }
+    })
+
+    return result
+  }
+
+  public position(element: HTMLElement, x: number, y: number) {
+    element.style.top = y + 'px'
+    element.style.left = x + 'px'
+  }
+
+  public console(
+    message: any,
+    type?: 'success' | 'waring' | 'error',
+    prefix = 'Btools'
+  ) {
+    let css: string
+
+    switch (type) {
+      case 'success':
+        prefix += ' Success:'
+        css =
+          'background-image: linear-gradient(to top, #0ba360 0%, #3cba92 100%);'
+        break
+      case 'waring':
+        prefix += ' Waring:'
+        css =
+          'background-image: linear-gradient(to top, #e6b980 0%, #eacda3 100%);'
+        break
+
+      case 'error':
+        prefix += ' Error:'
+        css =
+          'background-image: linear-gradient(to top, #ff0844 0%, #ffb199 100%);'
+        break
+
+      default:
+        prefix += ' Info:'
+        css =
+          'background: linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #989898; background-blend-mode: multiply,multiply;'
+    }
+
+    css += 'color: #FFF; padding: 2px 3px; border-radius: 3px;'
+
+    console.log('%c' + prefix, css, message)
+  }
+
+  /**
+   * 获取当前用户 cookie
+   * @param key cookie key
+   * @example
+   *
+   * ```javascript
+   * const csrf = await Util.Instance().getUserCookie('csrf')
+   * ```
+   */
+  public async getUserCookie(key: string): Promise<string | null> {
+    const userLocalData = await ExtStorage.Instance().getStorage<
+      TMultipleAccounts,
+      IMultipleAccounts
+    >(new TMultipleAccounts({
+      currentAccount: '',
+      userList: []
+    }))
+
+    const user = _.find(userLocalData.userList, {
+      uid: userLocalData.currentAccount
+    })
+
+    if (!user) return Promise.resolve(null)
+
+    return Promise.resolve(_.get(user, key, null))
+  }
+}
---
+++ src/scripts/base/storage/ExtStorage.ts
@@ -0,0 +1,73 @@
+/**
+ * 扩展本地存储
+ */
+
+import _ from 'lodash'
+
+import { browser } from 'webextension-polyfill-ts'
+
+import { TemplateBase } from '@/scripts/base/storage/template/TemplateBase'
+import Singleton from '@/scripts/base/singletonBase/Singleton'
+
+export default class ExtStorage extends Singleton {
+  public getStorage<T extends TemplateBase, TResult>(
+    configs: T
+  ): Promise<TResult> {
+    return this._getStorage<T, TResult>(configs)
+  }
+
+  public setStorage<T extends TemplateBase, TResult>(
+    configs: T
+  ): Promise<TResult> {
+    return this._setStorage<T, TResult>(configs)
+  }
+
+  public removeStorage<T extends TemplateBase, TResult>(
+    configs: T
+  ): Promise<TResult> {
+    const data = configs.GetData()
+    Object.keys(data).map(function (option: string | number) {
+      // 置空后保存
+      data[option] = null
+    })
+
+    configs.SetData(data)
+
+    return this._setStorage(configs)
+  }
+
+  public clear() {
+    browser.storage.local.clear()
+  }
+
+  private _getStorage<T extends TemplateBase, TResult>(
+    configs: T
+  ): Promise<TResult> {
+    return new Promise((resolve) => {
+      // 根据类型自动分配模块 index
+      const space = new Object()
+      space[configs.GetName()] = configs.GetData()
+
+      browser.storage.local.get(space).then((items) => {
+        resolve({
+          ...configs.GetData(),
+          ...items[configs.GetName()]
+        })
+      })
+    })
+  }
+
+  private _setStorage<T extends TemplateBase, TResult>(
+    configs: T
+  ): Promise<TResult> {
+    return new Promise((resolve) => {
+      // 根据类型自动分配模块 index
+      const space = {}
+      space[configs.GetName()] = configs.GetData()
+
+      browser.storage.local.set(space).then(() => {
+        resolve(configs.GetData() as TResult)
+      })
+    })
+  }
+}
